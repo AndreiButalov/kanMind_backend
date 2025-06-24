fcb68bf98fd217b16dad88c355de2b86be2735f8
AndrejYaotsin <andrej.butalov@gmx.de>
2025-06-24 11:29:26 +0200

    rename app

--- kanMind_app/apps.py
+++ kanMind_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KanmindBoardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kanMind_board'
+    name = 'kanMind_app'
