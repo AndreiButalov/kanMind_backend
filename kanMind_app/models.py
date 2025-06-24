db3159930d9d3a9a7fe658c97345e84e2e7c15be
AndrejYaotsin <andrej.butalov@gmx.de>
2025-06-24 12:24:10 +0200

    add task model

--- kanMind_app/models.py
+++ kanMind_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user_auth_app.models import UserProfile
 
-# Create your models here.
+class Task(models.Model):
+    # board
+    title = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, blank=True, null=True)
+    priority = models.CharField(max_length=20, default="medium")
+    reviewer = models.TextField(blank=True, null=True)
+    due_date = models.DateField()
+    comments = models.TextField()
+
