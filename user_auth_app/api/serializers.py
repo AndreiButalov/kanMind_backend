92c4d856c022d75fed3934fb29c7de81ad1e0e92
AndrejYaotsin <andrej.butalov@gmx.de>
2025-06-26 09:30:25 +0200

    add to user serializer id and fullname

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -9,28 +9,47 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
 
 
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from user_auth_app.models import UserProfile
+
 class UserProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    id = serializers.IntegerField(source='user.id', read_only=True)
+    fullname = serializers.CharField(source='user.username')
+    email = serializers.EmailField(source='user.email')
+    password = serializers.CharField(write_only=True)
+    repeated_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        fields = ['id', 'fullname', 'email', 'password', 'repeated_password']
+
+    def validate(self, data):
+        if data['password'] != data['repeated_password']:
+            raise serializers.ValidationError({'password': 'Passwörter stimmen nicht überein.'})
+        return data
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
-        user = User.objects.create_user(**user_data)
-        profile = UserProfile.objects.create(user=user, **validated_data)
+        password = validated_data.pop('password')
+        validated_data.pop('repeated_password')
+
+        user = User(username=user_data['username'], email=user_data['email'])
+        user.set_password(password)
+        user.save()
+
+        profile = UserProfile.objects.create(user=user)
         return profile
+
     
 
 class RegistrationSerializer(serializers.ModelSerializer):
     repeated_password = serializers.CharField(write_only=True)
-    color = serializers.CharField(write_only=True)
+    fullname = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'repeated_password']
-        # , 'color'
+        fields = ['fullname', 'email', 'password', 'repeated_password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate_email(self, value):
@@ -46,13 +65,12 @@ class RegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'password': 'Passwörter stimmen nicht überein.'})
         
         user = User(
-            username=self.validated_data['username'],
+            username=self.validated_data['fullname'],
             email=self.validated_data['email'],
         )
         user.set_password(pw)
         user.save()
         
-        # color = self.validated_data['color']
-        # UserProfile.objects.create(user=user, color=color)
+        UserProfile.objects.create(user=user)
 
         return user
