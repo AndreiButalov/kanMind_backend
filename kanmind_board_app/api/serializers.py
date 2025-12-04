881c4abe6be3d6b22b30f4c8edb721ef99e8271c
AndreiButalov <andrej.butalov@gmx.de>
2025-12-04 09:01:11 +0100

    add modelSerializer

--- kanmind_board_app/api/serializers.py
+++ kanmind_board_app/api/serializers.py
@@ -1,24 +1,11 @@
 from rest_framework import serializers
 from kanmind_board_app.models import Board
 
-class BoardSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    members = serializers.CharField(max_length=255)
-    members = serializers.CharField(max_length=255)
-    owner = serializers.CharField(max_length=255)
-
+class BoardSerializer(serializers.ModelSerializer):    
     class Meta:
         model = Board
-        fields = ['id', 'title', 'members', 'owner']
+        fields = '__all__'
 
-    def create(self, validated_data):
-        return Board.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.members = validated_data.get('members', instance.members)
-        instance.owner = validated_data.get('owner', instance.owner)
-        instance.save()
-        return instance
     
     
--- kanmind_board_app/models.py
+++ kanmind_board_app/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Board(models.Model):
     title = models.TextField(max_length=255)
     members = models.TextField(max_length=255)
-    owner =models.TextField(max_length=255)
+    owner = models.TextField(max_length=255)
