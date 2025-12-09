29ab6b1b787800de5a7cf8895c26a3d33745bd98
AndreiButalov <andrej.butalov@gmx.de>
2025-12-09 11:48:01 +0100

    add class BoardView/boardSingleView

--- kanmind_board_app/api/views.py
+++ kanmind_board_app/api/views.py
@@ -14,23 +14,7 @@ class BoardsView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Generi
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
-# @api_view(['GET', 'POST'])
-# def boards_view(request):
-
-#     if request.method == 'GET':
-#         boards = Board.objects.all()
-#         serializer = BoardSerializer(boards, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#     if request.method == 'POST':
-#         serializer = BoardSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+   
 
 class BoardSingleView(
             mixins.RetrieveModelMixin,
@@ -50,32 +34,6 @@ class BoardSingleView(
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
-
-
-
-# @api_view(['GET', 'DELETE', 'PUT'])
-# def board_single_view(request, pk):
-
-#     if request.method == 'GET':
-#         board = Board.objects.get(pk=pk)
-#         serializer = BoardSerializer(board)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-#     if request.method == 'DELETE':
-#         board = Board.objects.get(pk=pk)
-#         serializer = BoardSerializer(board)
-#         board.delete()
-#         return Response(serializer.data)
-    
-#     if request.method == 'PUT':
-#         board = Board.objects.get(pk=pk)
-#         serializer = BoardSerializer(board, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
 
 
 @api_view(['GET'])
