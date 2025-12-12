a77df204cbc96a0e17d48f8def51e17af505912d
AndreiButalov <andrej.butalov@gmx.de>
2025-12-12 09:58:00 +0100

    add Login/urls change

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserProfileList, UserProfileDetail, RegisrationView
+from .views import UserProfileList, UserProfileDetail, RegisrationView, LoginView
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
     path('registration/', RegisrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login')
 ]
