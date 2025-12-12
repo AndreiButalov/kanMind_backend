19e11850724b0272a20e9382ddf0a9ef5de392d7
AndreiButalov <andrej.butalov@gmx.de>
2025-12-12 11:56:55 +0100

    delete 'bio', 'location'

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -2,9 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
-    location = models.CharField(max_length=100, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')    
 
     def __str__(self):
         return self.user.username
