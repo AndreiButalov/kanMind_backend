0b98ad11b094258a052edc93f290a73887b5517c
AndreiButalov <andrej.butalov@gmx.de>
2025-12-16 13:38:34 +0100

    add members_count

--- kanmind_board_app/api/serializers.py
+++ kanmind_board_app/api/serializers.py
@@ -5,12 +5,17 @@ from django.contrib.auth.models import User
 class BoardSerializer(serializers.ModelSerializer):
     members = serializers.PrimaryKeyRelatedField(
         many=True,
-        queryset=User.objects.all()
+        queryset=User.objects.all(),
+        write_only=True
     )
 
+    member_count = serializers.SerializerMethodField()
     class Meta:
         model = Board
-        fields = '__all__'
+        fields = ['id', 'members', 'title', 'member_count']
+
+    def get_member_count(self, obj):
+        return obj.members.count()
 
     
 
