ca93e809ff0144cc9430875ca53bb88178d696ed
AndreiButalov <andrej.butalov@gmx.de>
2025-12-16 09:55:05 +0100

    del/add DB.sqlite

--- kanmind_board_app/admin.py
+++ kanmind_board_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Board, Task, Comment
 
-class BoardAdmin(admin.ModelAdmin):
-    list_filter=['title']
-    # list_display=['title', 'members', 'owner']
+# class BoardAdmin(admin.ModelAdmin):
+#     list_filter=['title']
+#     # list_display=['title', 'members', 'owner']
 
-admin.site.register(Board, BoardAdmin)
+admin.site.register(Board)
 admin.site.register(Task)
 admin.site.register(Comment)
