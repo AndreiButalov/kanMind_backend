271513d9c5265ffcaa29b9f91a224800868d5a7b
AndreiButalov <andrej.butalov@gmx.de>
2025-12-22 17:56:19 +0100

    CommentsSerializer->update, commentsView->update, commentDeleteView->update

--- kanmind_board_app/api/views.py
+++ kanmind_board_app/api/views.py
@@ -142,11 +142,9 @@ class CommentsView(
     def get_queryset(self):
         return Comment.objects.filter(task_id=self.kwargs['task_id'])
 
-    # ✅ GET MUSS EXPLIZIT DA SEIN
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-    # ✅ POST MIT AUTHOR + TASK
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
