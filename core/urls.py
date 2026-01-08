9987e78375420206f0855fcb6fed239c2679fdfd
AndreiButalov <andrej.butalov@gmx.de>
2026-01-08 17:25:32 +0100

    kanmind_board->rename core

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kanmind_board project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('kanmind_board_app.api.urls')),
-    # path('api-auth', include ('rest_framework.urls'))
     path('api/', include('user_auth_app.api.urls')),
 ]
